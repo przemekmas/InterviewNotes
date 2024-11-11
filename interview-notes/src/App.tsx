a5f7b2ba31c36182131cfa1bb566daafdf49a6fe
Przemyslaw Maslowski <przemyslawm@outlook.com>
2024-11-11 23:27:54 +0000

    split up the json files into their own topics

--- interview-notes/src/App.tsx
+++ interview-notes/src/App.tsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import data from './notes.json';
+import apiNoteData from './notes/apiNotes.json';
+import azureCloudNoteData from './notes/azureCloudNotes.json';
+import csharpNoteData from './notes/csharpNotes.json';
+import sqlNoteData from './notes/sqlNotes.json';
 import './App.css';
 import Note from './components/Note';
 import { NoteType } from './types/NoteType';
 import Button from '@mui/material/Button';
-import { Grid2, Typography, Container, createTheme, ThemeProvider, Box } from '@mui/material';
+import { Grid2, Typography, Container, createTheme, ThemeProvider, Box, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 
 function App() {
   const [noteIndex, setNoteIndex] = useState<number>(0);
   const [currentNote, setCurrentNote] = useState<NoteType>();
+  const [topicValue, setTopicValue] = React.useState('csharp');
+  const [data, setData] = useState<Array<NoteType>>(csharpNoteData);
 
   useEffect(() => {
     if (data) {
@@ -38,6 +43,26 @@ function App() {
     setCurrentNote(data[noteIndex]);
   }, [noteIndex]);
 
+  const handleTopicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    var newTopicValue = (event.target as HTMLInputElement).value;
+    setNoteIndex(0);
+    setTopicValue(newTopicValue);
+    switch (newTopicValue) {
+      case "csharp":
+        setData(csharpNoteData);
+        break;
+      case "api":
+        setData(apiNoteData);
+        break;
+      case "azureCloud":
+        setData(azureCloudNoteData);
+        break;
+      case "sql":
+        setData(sqlNoteData);
+        break;
+    }
+  };
+
   const darkTheme = createTheme({
     palette: {
       mode: 'dark',
@@ -64,6 +89,23 @@ function App() {
         <NavBar></NavBar>
         <Container className='AppContent' sx={{ marginBottom: 3 }}>
           <Grid2 container spacing={4}>
+            <Grid2 size={12}>
+              <FormControl>
+                <FormLabel id="demo-row-radio-buttons-group-label">Topic</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                  value={topicValue}
+                  onChange={handleTopicChange}
+                >
+                  <FormControlLabel value="csharp" control={<Radio />} label="C#" />
+                  <FormControlLabel value="api" control={<Radio />} label=".NET API" />
+                  <FormControlLabel value="azureCloud" control={<Radio />} label="Azure Cloud" />
+                  <FormControlLabel value="sql" control={<Radio />} label="SQL" />
+                </RadioGroup>
+              </FormControl>
+            </Grid2>
             <Grid2 size={12}>
               <Typography>Question: <b>{noteIndex + 1}</b> of <b>{data.length}</b></Typography>
             </Grid2>
